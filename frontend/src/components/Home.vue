0ebfceb84b2183f48f381fe82ea005e591032681
Simon Gschnell <Simon.Gschnell@stud-inf.unibz.it>
2020-03-14 10:52:17 +0100

    vue-flask template

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>Home</p>
+    <p>Random number from backend: {{ randomNumber }}</p>
+    <button @click="getRandom">New random number</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default{
+  data(){
+    return{
+      randomNumber: 0
+    }
+  },
+  methods: {
+    getRandom () {
+      this.randomNumber = this.getRandomNumberFromBackend()
+    },
+    getRandomNumberFromBackend () {
+      const path = 'http://localhost:5000/api/random'
+      axios.get(path).then( response => {
+        this.randomNumber = response.data.randomNumber
+      }).catch( error => {
+        console.log(error)
+      })
+    }
+  }, 
+  created () {
+    this.getRandom()
+  } 
+}
+</script>
---
+++ frontend/src/components/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p>Error404</p>
+  </div>
+</template>
+
+
