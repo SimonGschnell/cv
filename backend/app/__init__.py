e61ef0830776c75ab834457e805ab78bba4db655
Simon Gschnell <Simon.Gschnell@stud-inf.unibz.it>
2020-04-05 19:49:34 +0200

    heroku

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 
 app = Flask(__name__, static_folder="../../dist/static", template_folder="../../dist")
 
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app)
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
